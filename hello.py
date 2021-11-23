a563637649a7fc9e5f125eda5f631b3d04af9403
aram5130 <starhigu@gmail.com>
2021-11-23 20:04:07 +0900

    merge fail ....

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-j = 1
-if j==1:
-    print("This is {} from conflict1 branch".format(j))
-
+for j in range(10):
+    if j%2==0:
+    print("This is {} from conflict1 and main branch".format(j))
+    else:
+    print(J)
